55144a1939e140cc409c80fb0b2dfa58b92e5a32
Takayama Fumihiko <tekezo@pqrs.org>
2016-11-19 15:47:21 +0900

    suppress kextunload error messages

--- src/scripts/unload.sh
+++ src/scripts/unload.sh
@@ -4,7 +4,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH
 
 # --------------------------------------------------
 for kext in $(kextstat | ruby -ne 'print $1,"\n" if /(org\.pqrs\.driver\.VirtualHIDManager.*?) /'); do
-    sudo kextunload -b $kext
+    sudo kextunload -b $kext -q
 done
 
 exit 0
