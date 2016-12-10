9440f19f44bbc827478585de85bbdaf79bda0363
Takayama Fumihiko <tekezo@pqrs.org>
2016-12-10 23:16:35 +0900

    remove virtualHIDEventService_ from VirtualHIDRoot

--- src/VirtualHIDRoot/VirtualHIDRoot.cpp
+++ src/VirtualHIDRoot/VirtualHIDRoot.cpp
@@ -3,43 +3,12 @@
 #define super IOService
 OSDefineMetaClassAndStructors(VIRTUAL_HID_ROOT_CLASS, super);
 
-bool VIRTUAL_HID_ROOT_CLASS::init(OSDictionary* dictionary) {
-  if (!super::init(dictionary)) {
-    return false;
-  }
-
-  virtualHIDEventService_ = nullptr;
-
-  return true;
-}
-
 bool VIRTUAL_HID_ROOT_CLASS::start(IOService* provider) {
   IOLog(VIRTUAL_HID_ROOT_CLASS_STRING "::start\n");
 
   if (!super::start(provider)) {
     return false;
   }
-
-  do {
-    virtualHIDEventService_ = new VIRTUAL_HID_EVENT_SERVICE_CLASS();
-    if (virtualHIDEventService_) {
-      if (virtualHIDEventService_->init(nullptr)) {
-        if (virtualHIDEventService_->attach(this)) {
-          if (virtualHIDEventService_->start(this)) {
-            IOLog(VIRTUAL_HID_ROOT_CLASS_STRING "::virtualHIDEventService_ is started\n");
-            break;
-          }
-
-          // Error handling
-          virtualHIDEventService_->detach(this);
-        }
-      }
-      // Error handling
-      virtualHIDEventService_->release();
-      virtualHIDEventService_ = nullptr;
-    }
-
-  } while (true);
 
   // Publish ourselves so clients can find us
   //
@@ -53,12 +22,6 @@ bool VIRTUAL_HID_ROOT_CLASS::start(IOService* provider) {
 
 void VIRTUAL_HID_ROOT_CLASS::stop(IOService* provider) {
   IOLog(VIRTUAL_HID_ROOT_CLASS_STRING "::stop\n");
-
-  if (virtualHIDEventService_) {
-    virtualHIDEventService_->terminate(kIOServiceSynchronous);
-    virtualHIDEventService_->release();
-    virtualHIDEventService_ = nullptr;
-  }
 
   super::stop(provider);
 }
--- src/VirtualHIDRoot/VirtualHIDRoot.hpp
+++ src/VirtualHIDRoot/VirtualHIDRoot.hpp
@@ -8,20 +8,10 @@ BEGIN_IOKIT_INCLUDE;
 #include <IOKit/IOService.h>
 END_IOKIT_INCLUDE;
 
-#include "VirtualHIDEventService.hpp"
-
 class VIRTUAL_HID_ROOT_CLASS final : public IOService {
   OSDeclareDefaultStructors(VIRTUAL_HID_ROOT_CLASS);
 
 public:
-  virtual bool init(OSDictionary* dictionary = nullptr) override;
   virtual bool start(IOService* provider) override;
   virtual void stop(IOService* provider) override;
-
-  VIRTUAL_HID_EVENT_SERVICE_CLASS* getVirtualHIDEventService(void) const {
-    return virtualHIDEventService_;
-  }
-
-private:
-  VIRTUAL_HID_EVENT_SERVICE_CLASS* virtualHIDEventService_;
 };
