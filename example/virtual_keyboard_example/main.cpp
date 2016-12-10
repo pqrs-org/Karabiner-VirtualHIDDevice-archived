b9d32d76a37c088a993efa2c586ffd10d271fb8e
Takayama Fumihiko <tekezo@pqrs.org>
2016-12-11 01:09:37 +0900

    remove log message

--- example/virtual_keyboard_example/main.cpp
+++ example/virtual_keyboard_example/main.cpp
@@ -93,7 +93,6 @@ int main(int argc, const char* argv[]) {
   std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
   std::cout << std::endl;
-  std::cout << "reset_virtual_hid_keyboard is called" << std::endl;
 
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
