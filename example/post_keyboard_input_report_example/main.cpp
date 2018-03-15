5d94c27580f13a588e13375f39b0de107a1e2b8f
Takayama Fumihiko <tekezo@pqrs.org>
2018-03-16 02:29:03 +0900

    add consumer,apple_vendor_top_case example into post_keyboard_input_report_example

--- example/post_keyboard_input_report_example/main.cpp
+++ example/post_keyboard_input_report_example/main.cpp
@@ -31,6 +31,8 @@ int main(int argc, const char* argv[]) {
     goto finish;
   }
 
+  // keyboard_input
+
   {
     pqrs::karabiner_virtual_hid_device::properties::keyboard_initialization properties;
     properties.keyboard_type = pqrs::karabiner_virtual_hid_device::properties::keyboard_type::ansi;
@@ -72,13 +74,69 @@ int main(int argc, const char* argv[]) {
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
 
+  // consumer_input (option+mute)
+
   {
     pqrs::karabiner_virtual_hid_device::hid_report::keyboard_input report;
-    report.report_id = 2;
-    report.modifiers = 0;
-    report.keys[0] = kHIDUsage_Csmr_Mute;
+    report.modifiers = pqrs::karabiner_virtual_hid_device::hid_report::keyboard_input::modifier::left_option;
 
     kr = pqrs::karabiner_virtual_hid_device_methods::post_keyboard_input_report(connect, report);
+    if (kr != KERN_SUCCESS) {
+      std::cerr << "post_keyboard_input_report error" << std::endl;
+    }
+  }
+  {
+    pqrs::karabiner_virtual_hid_device::hid_report::consumer_input report;
+    report.keys[0] = kHIDUsage_Csmr_Mute;
+
+    kr = pqrs::karabiner_virtual_hid_device_methods::post_consumer_input_report(connect, report);
+    if (kr != KERN_SUCCESS) {
+      std::cerr << "post_consumer_input_report error" << std::endl;
+    }
+  }
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+  // apple_vendor_top_case_input (fn+spacebar)
+
+  {
+    pqrs::karabiner_virtual_hid_device::hid_report::apple_vendor_top_case_input report;
+    report.keys[0] = 0x0003; // kHIDUsage_AV_TopCase_KeyboardFn
+
+    kr = pqrs::karabiner_virtual_hid_device_methods::post_apple_vendor_top_case_input_report(connect, report);
+    if (kr != KERN_SUCCESS) {
+      std::cerr << "post_apple_vendor_top_case_input_report error" << std::endl;
+    }
+  }
+  {
+    pqrs::karabiner_virtual_hid_device::hid_report::keyboard_input report;
+    report.keys[0] = kHIDUsage_KeyboardSpacebar;
+
+    kr = pqrs::karabiner_virtual_hid_device_methods::post_keyboard_input_report(connect, report);
+    if (kr != KERN_SUCCESS) {
+      std::cerr << "post_keyboard_input_report error" << std::endl;
+    }
+  }
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+  // apple_vendor_keyboard_input (command+mission_control)
+
+  {
+    pqrs::karabiner_virtual_hid_device::hid_report::keyboard_input report;
+    report.modifiers = pqrs::karabiner_virtual_hid_device::hid_report::keyboard_input::modifier::left_command;
+
+    kr = pqrs::karabiner_virtual_hid_device_methods::post_keyboard_input_report(connect, report);
+    if (kr != KERN_SUCCESS) {
+      std::cerr << "post_keyboard_input_report error" << std::endl;
+    }
+  }
+
+  {
+    pqrs::karabiner_virtual_hid_device::hid_report::apple_vendor_keyboard_input report;
+    report.keys[0] = 0x0010; // kHIDUsage_AppleVendorKeyboard_Expose_All
+
+    kr = pqrs::karabiner_virtual_hid_device_methods::post_apple_vendor_keyboard_input_report(connect, report);
     if (kr != KERN_SUCCESS) {
       std::cerr << "post_keyboard_input_report error" << std::endl;
     }
